26eef9ceb1a6f57f3d6f99cfd95ceab8d5365cde
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-12-05 23:35:11 +0300

    tests still failing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/plants', to: 'plants#index'
+  get '/plants/:id', to: 'plants#show'
+
+   #Actions
+   resource :plants, only: [:show, :create]
 end
